70cd315033a1b3229a4d33315cd0d32f9454e0b7
devUserContact <developer.user.contact@protonmail.com>
2022-05-20 18:58:46 -0700

    linting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
-import SearchForm from './SearchForm'
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import SearchForm from "./SearchForm";
 
 const Home: NextPage = () => {
   return (
@@ -13,13 +13,12 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-				<SearchForm />
+        <SearchForm />
       </main>
 
-      <footer className={styles.footer}>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
