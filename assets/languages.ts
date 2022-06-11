bdb11a1004003bcf8be0a01c0e19af3c52a985f2
devUserContact <developer.user.contact@protonmail.com>
2022-06-10 19:02:24 -0700

    changed json object array to typescript object array

---
+++ assets/languages.ts
@@ -0,0 +1,20 @@
+export interface Language {
+  language: string;
+  url: string | string[];
+  checked: boolean;
+}
+export const languages: Language[] = [{
+  language: 'korean' ,
+  url: 'https://en.dict.naver.com/#/search?query=',
+  checked: false 
+},
+{
+  language: 'japanese' ,
+  url: 'https://jisho.org/search/',
+  checked: false 
+},
+{
+  language: 'mandarin' ,
+  url: ['https://mandarinspot.com/dict?word=', '&phs=pinyin&sort=rel'],
+  checked: false 
+}]
