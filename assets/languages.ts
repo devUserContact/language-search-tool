c0ded4b7e691e5c641a7d75e7e9a280b2990ff8a
devUserContact <developer.user.contact@protonmail.com>
2022-06-15 22:36:46 -0700

    added language options to object array

--- assets/languages.ts
+++ assets/languages.ts
@@ -3,19 +3,35 @@ export interface Language {
   url: string | string[];
   checked: boolean;
 }
-export const languages: Language[] = [{
-  language: 'korean' ,
-  url: 'https://en.dict.naver.com/#/search?query=',
-  checked: false 
-},
-{
-  language: 'japanese' ,
-  url: 'https://jisho.org/search/',
-  checked: false 
-},
-//{
-//  language: 'mandarin' ,
-//  url: ['https://mandarinspot.com/dict?word=', '&phs=pinyin&sort=rel'],
-//  checked: false 
-//}
-]
+export const languages: Language[] = [
+  {
+    language: "french",
+    url: "https://dictionary.cambridge.org/dictionary/english-french/",
+    checked: false,
+  },
+  {
+    language: "hebrew",
+    url: "https://en.glosbe.com/en/he/",
+    checked: false,
+  },
+  {
+    language: "korean",
+    url: "https://en.dict.naver.com/#/search?query=",
+    checked: false,
+  },
+  {
+    language: "japanese",
+    url: "https://jisho.org/search/",
+    checked: false,
+  },
+  {
+    language: "ukrainian",
+    url: "https://en.glosbe.com/en/uk/",
+    checked: false,
+  },
+  //{
+  //  language: 'mandarin' ,
+  //  url: ['https://mandarinspot.com/dict?word=', '&phs=pinyin&sort=rel'],
+  //  checked: false
+  //}
+];
